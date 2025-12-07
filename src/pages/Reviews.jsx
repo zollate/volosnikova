2038114c2e3421473ef8e658d19bd97af8be395a
zollate <zollate2006@mail.ru>
2025-12-07 19:45:53 +0500

    artem govoril delali

---
+++ src/pages/Reviews.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '../components/Button'
+
+function Reviews() {
+  return (
+    <div><Button>                       
+        mama
+    </Button></div>
+  )
+}
+
+export default Reviews
