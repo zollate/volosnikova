ca661fd8725a464827d26b7c0dd927818bc251ba
Hayabusa-cc <igra.igrov1064@gmail.com>
2025-12-09 12:35:08 +0500

    добавлен компонент карточки каталога

---
+++ src/components/CatalogCard/CatalogCard.jsx
@@ -0,0 +1,14 @@
+import './CatalogCard.scss';
+
+const CatalogCard = ({ title, price, image }) => {
+  return (
+    <div className="catalog-card">
+      <img src={image} alt={title} className="catalog-card__image"/>
+      <h3 className="catalog-card__title">{title}</h3>
+      <p className="catalog-card__price">{price} ₽</p>
+      <button className="catalog-card__button">Добавить в корзину</button>
+    </div>
+  );
+}
+
+export default CatalogCard;
