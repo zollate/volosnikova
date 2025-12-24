b0e22df19206d4e3bf694286ace41774a8cf8b12
Hayabusa-cc <igra.igrov1064@gmail.com>
2025-12-24 22:26:44 +0500

    починил карточки товара

--- src/layouts/Content/Content.jsx
+++ src/layouts/Content/Content.jsx
@@ -3,6 +3,8 @@ import Home from '../../pages/Home';
 import ReviewsPage from '../../pages/ReviewsPage';
 import Catalog from '../../pages/Catalog';
 import News from '../../pages/News/News.jsx';
+// 1. ИМПОРТИРУЕМ СТРАНИЦУ ТОВАРА
+import CatalogItemPage from '../../pages/CatalogItemPage/CatalogItemPage'; 
 
 function Content() {
   return (
@@ -11,6 +13,11 @@ function Content() {
         <Route path="/" element={<Home />} />
         <Route path="/reviews" element={<ReviewsPage />} />
         <Route path="/catalog" element={<Catalog />} />
+        
+        {/* 2. ДОБАВЛЯЕМ ЭТОТ МАРШРУТ */}
+        {/* :id позволяет роутеру понимать, что после /item/ может быть любое число */}
+        <Route path="/catalog/item/:id" element={<CatalogItemPage />} />
+        
         <Route path="/news" element={<News />} />
       </Routes>
     </main>
